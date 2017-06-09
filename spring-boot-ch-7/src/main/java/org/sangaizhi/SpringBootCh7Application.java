45b1b216b20e683fec3c8b8b85df9920106eb34f
sangaizhi <sangaizhi@163.com>
2017-06-09 18:40:15 +0800

    Spring Boot 配置 Tomcat

---
+++ spring-boot-ch-7/src/main/java/org/sangaizhi/SpringBootCh7Application.java
@@ -0,0 +1,12 @@
+package org.sangaizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCh7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCh7Application.class, args);
+	}
+}
